430a5430648bbeb59463c55eab7eddf94f1a9161
dream <1574775812@qq.com>
2018-12-30 00:15:07 +0800

    animals修改

--- 14_animals/animals/animals.h
+++ 14_animals/animals/animals.h
@@ -1,4 +1,62 @@
 #ifndef ANIMALS_H
 #define ANIMALS_H
 
+#include "iostream"
+#include<string>
+using namespace std;
+class Animal
+{
+    protected:
+    string name;
+public:
+    Animal(string nam):name(nam){}
+    virtual void cry() = 0;
+};
+
+class Mouse : public Animal
+{
+private:
+    char sex;
+public:
+    Mouse(string nam, char s):Animal(nam),sex(s) {}
+    virtual void cry()
+    {
+        cout<<"我叫"<<name<<"，是一只"<<((sex=='m')?"男":"女")<<"老鼠，我的叫声是：吱吱吱！"<<endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    Cat(string nam):Animal(nam) {}
+    virtual void cry()
+    {
+        cout<<"我叫"<<name<<"，是一只猫，我的叫声是：喵喵喵！"<<endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    Dog(string nam):Animal(nam) {}
+    virtual void cry()
+    {
+        cout<<"我叫"<<name<<"，是一条狗，我的叫声是：汪汪汪！"<<endl;
+    }
+};
+
+class Giraffe : public Animal
+{
+private:
+    char sex;
+public:
+    Giraffe(string nam,char s):Animal(nam), sex(s) {}
+    virtual void cry()
+    {
+        cout<<"我叫"<<name<<"，是"<<((sex=='m')?"男":"女")<<"长颈鹿，我的脖子太长，发不出声音来！"<<endl;
+    }
+};
+
+
+
 #endif // ANIMALS_H
--- 14_animals/animals/main.cpp
+++ 14_animals/animals/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
+#include "animals.h"
 using namespace std;
 
-int main()
+
+int main( )
 {
-    cout << "Hello World!" << endl;
+    Animal *p;
+//    p = new Animal();
+//    p->cry(); //输出: 不知哪种动物，让我如何学叫？
+    Mouse m1("Jerry",'m');
+    p=&m1;
+    p->cry(); //输出: 我叫Jerry，是一只男老鼠，我的叫声是：吱吱吱！
+    Mouse m2("Jemmy",'f');
+    p=&m2;
+    p->cry(); //输出: 我叫Jemmy，是一只女老鼠，我的叫声是：吱吱吱！
+    Cat c1("Tom");
+    p=&c1;
+    p->cry(); //输出: 我叫Tom，是一只猫猫，我的叫声是：喵喵喵！
+    Dog d1("Droopy");
+    p=&d1;
+    p->cry(); //输出: 我叫Droopy，是一条狗狗，我的叫声是：汪汪汪！
+    Giraffe g1("Gill",'m');
+    p=&g1;
+    p->cry(); //输出: 我叫Gill，是男长颈鹿，脖子太长，发不出声音来！
     return 0;
 }
